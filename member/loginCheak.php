1006e98138085ab79abd2bf79f0c6775d3435e03
sangmin Jung <skswoddl1014@daum.net>
2022-01-07 14:31:40 +0900

    login cheak 완료

---
+++ member/loginCheak.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>스마일푸드 야미</title>
+</head>
+<body>
+<?php
+    print('<h1>입력한 정보</h1>'); 
+    print($_POST['userId']);
+    print( $_POST['userPw']);
+    ?>
+</body>
+</html>
